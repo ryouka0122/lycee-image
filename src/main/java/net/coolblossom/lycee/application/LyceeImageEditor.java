900afe92010eaa0f67c826b15a6656c1a9f7ca14
Ryouka <coolblossom.0130@gmail.com>
2016-12-03 10:59:53 +0900

    保存

--- src/main/java/net/coolblossom/lycee/application/LyceeImageEditor.java
+++ src/main/java/net/coolblossom/lycee/application/LyceeImageEditor.java
@@ -1,6 +1,9 @@
 package net.coolblossom.lycee.application;
 
+import java.awt.FileDialog;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 import net.coolblossom.lycee.guis.MainWindow;
@@ -8,12 +11,20 @@ import net.coolblossom.lycee.utils.ImageUtil;
 import net.coolblossom.lycee.utils.PathUtil;
 import net.coolblossom.lycee.utils.ResourcesUtil;
 
+/**
+ * アプリケーション本体
+ * @author ryouka0122@github
+ *
+ */
 public class LyceeImageEditor extends AbstractApplication {
 
+	/** メインウィンドウ */
 	MainWindow window;
 
+	/** 読み込んだ画像パス */
 	String path;
 
+	/** オリジナル画像 */
 	Image imgOriginal;
 
 	@Override
@@ -29,7 +40,17 @@ public class LyceeImageEditor extends AbstractApplication {
 		} catch (IOException e) {
 			return false;
 		}
+
+		window.addKeyListener(this);
+		window.addWindowListener(this);
 		return true;
+	}
+
+	void openImageDialog() {
+		FileDialog dialog = new FileDialog(window, "ファイルを開く", FileDialog.LOAD);
+		dialog.setVisible(true);
+		System.out.println("dictory :" + dialog.getDirectory());
+		System.out.println("file    : " + dialog.getFile());
 	}
 
 	@Override
@@ -43,5 +64,19 @@ public class LyceeImageEditor extends AbstractApplication {
 		;
 	}
 
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		switch(e.getKeyChar()) {
+		case 'o':
+			openImageDialog();
+			break;
+		}
+		//System.out.println("KeyCode:" + e.getKeyChar());
+	}
 
 }
